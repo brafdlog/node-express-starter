c506642d76a71a69c3a0acf5f015bc6e14ab132d
Jonathan <jonathan@dudamobile.com>
2018-04-10 19:53:17 +0300

    Add prettier

--- app.js
+++ app.js
@@ -19,14 +19,16 @@ app.set('view engine', 'hbs');
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
